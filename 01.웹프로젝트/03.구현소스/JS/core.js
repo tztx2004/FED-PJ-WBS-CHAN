b2b72d6cc44693b3e8d7dbecfbae53d08729e8c6
tztx2004 <tztx2004@gmail.com>
2023-03-16 00:20:39 +0900

    updated

--- 01.웹프로젝트/03.구현소스/JS/core.js
+++ 01.웹프로젝트/03.구현소스/JS/core.js
@@ -17,45 +17,51 @@ window.addEventListener("DOMContentLoaded",()=>{
     const icon = document.querySelector(".c")
     const bag = document.querySelector(".bag");
     const bag_c = document.querySelector(".bag_comment");
+    const search_imgbx = document.querySelector(".search_imgbx")
 
+    const gnb_a = document.querySelectorAll(".gnb a");
 
+    /* gnb 닿을 때 .top으로 배경 채워줌 */
     for(let x of List){
         x.onmouseover = ()=>{
-            top.style.height = "150px";
-        }
+            top.classList.add("on");
+        } //////////// onmouseover //////////////
         x.onmouseleave = ()=>{
-            top.style.height = "44px";
-        }
+            top.classList.remove("on");
+            bag_c.classList.remove("on");
+        }//////////// onmouseleave ////////////
     } //////////////// for of ////////////////////
 
 
     /***************************************** 
-        기능 : 검색바 오버시 gnb 투명해짐
+        기능 : 검색바 오버시 검색바 크기에 의해 .search의 right값 수정
     *****************************************/
     search.onmouseover = ()=>{
         // header_main.style.opacity = 0;
-        header_right.style.width = "40vw";
+        search.style.right = "15%";
     }; /////////////// onmouseover ///////////////////
     search.onmouseout = ()=>{
         if(search_bar.value.length === 0) {
-        header_right.style.width = "25vw";
-        }
-    }; /////////////// onmouseleave ///////////////////
+        search.style.right = "19%";
+        } /////////// if /////////////
+    }; /////////////// onmouseout ///////////////////
     
     
     /************************************* 
         장바구니 클릭 시 장바구니 창 나옴
     *************************************/
     bag.onclick = ()=>{
-        top.classList.add("on");
-        setTimeout(()=>{
-            bag_c.classList.add("on");
-        },200);
-        
-        // if(top.classList.contains("on") === true){
-        //         top.classList.remove("on");
-        //         bag_c.classList.remove("on");
-        //     }
+        if(top.classList.contains("on")){
+            top.classList.remove("on");
+            bag_c.classList.remove("on");
+        } ///////////// if ///////////////
+        else{
+            console.log("장바구니 눌럿음")
+            top.classList.add("on");
+            setTimeout(()=>{
+                bag_c.classList.add("on");
+            },200); ///// setTimeout //////
+        } ////////// else ///////////
     }; //////////////// onclick ////////////////
     // console.log(top.classList.contains("on"));
     
@@ -69,7 +75,9 @@ window.addEventListener("DOMContentLoaded",()=>{
     
 
     /******************************************* 
-        기능 : 
+        기능 : .gnb a 오버 시 약간 커짐
     *******************************************/
+    
+
 
 }); ///////////////// 로드구역 ////////////////////////
