cb8bfc8e2c2c071de3e18a1d84ee5b2cc34c79f8
tztx2004 <tztx2004@gmail.com>
2023-03-15 17:20:00 +0900

    updated

--- 01.웹프로젝트/03.구현소스/JS/core.js
+++ 01.웹프로젝트/03.구현소스/JS/core.js
@@ -5,12 +5,29 @@ window.addEventListener("DOMContentLoaded",()=>{
     console.log("core");
 
     // 대상
+    // 필요 대상
+    const List = document.querySelectorAll(".gnb>ul>li");
+    const sub_menu = document.querySelectorAll(".sub_menu");
+
+    const top = document.querySelector(".top");
     const search = document.querySelector(".search");
     const search_bar = document.querySelector(".search_bar");
     const header_main = document.querySelector(".header_main");
     const header_right = document.querySelector(".header_right");
-
+    const icon = document.querySelector(".c")
     const bag = document.querySelector(".bag");
+    const bag_c = document.querySelector(".bag_comment");
+
+
+    for(let x of List){
+        x.onmouseover = ()=>{
+            top.style.height = "150px";
+        }
+        x.onmouseleave = ()=>{
+            top.style.height = "44px";
+        }
+    } //////////////// for of ////////////////////
+
 
     /***************************************** 
         기능 : 검색바 오버시 gnb 투명해짐
@@ -19,15 +36,37 @@ window.addEventListener("DOMContentLoaded",()=>{
         // header_main.style.opacity = 0;
         header_right.style.width = "40vw";
     }; /////////////// onmouseover ///////////////////
-    search.onmouseleave = ()=>{
+    search.onmouseout = ()=>{
         if(search_bar.value.length === 0) {
-            header_right.style.width = "25vw";
-            header_main.style.opacity = 1;
+        header_right.style.width = "25vw";
         }
-        if(search_bar.value.length > 0) header_main.style.opacity = 0;
     }; /////////////// onmouseleave ///////////////////
     
-
+    
+    /************************************* 
+        장바구니 클릭 시 장바구니 창 나옴
+    *************************************/
+    bag.onclick = ()=>{
+        top.classList.add("on");
+        setTimeout(()=>{
+            bag_c.classList.add("on");
+        },200);
+        
+        // if(top.classList.contains("on") === true){
+        //         top.classList.remove("on");
+        //         bag_c.classList.remove("on");
+        //     }
+    }; //////////////// onclick ////////////////
+    // console.log(top.classList.contains("on"));
+    
+    /* 마우스 벗어나면 장바구니 들어감 */
+    top.onmouseleave = ()=>{
+        // console.log("leave");
+        top.classList.remove("on");
+        bag_c.classList.remove("on");
+    };
+    
+    
 
     /******************************************* 
         기능 : 
--- 01.웹프로젝트/03.구현소스/JS/main.js
+++ 01.웹프로젝트/03.구현소스/JS/main.js
@@ -1,28 +1,13 @@
 window.addEventListener("DOMContentLoaded",()=>{
     
-    // 필요 대상
-    const top = document.querySelector(".top");
-    const List = document.querySelectorAll(".gnb>ul>li");
-    const sub_menu = document.querySelectorAll(".sub_menu");
-    const search = document.querySelector(".search img");
+    
 
     const gbtn = document.querySelectorAll(".gbtn");
     const btn_n = document.querySelector(".btn_n")
     const btn_p = document.querySelector(".btn_p")
     const gimg = document.querySelector(".gimg");
-    // 상단바 배경
     
-    for(let x of List){
-        x.style.transition = ".2s";
-        x.onmouseover = ()=>{
-            top.style.height = "150px";
-            top.style.transition = ".2s";
-        }
-        x.onmouseleave = ()=>{
-            top.style.height = "44px";
-        }
-    } //////////////// for of ////////////////////
-
+    
     // search.addEventListener("click",()=>{
     //     top.style.height = "150px";
     //     top.style.transition = ".2s";
