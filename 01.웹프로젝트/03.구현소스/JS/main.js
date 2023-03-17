f71162e7b64af41c70ce0e45efe9d64a1ef75978
tztx2004 <tztx2004@gmail.com>
2023-03-18 01:38:04 +0900

    updated

--- 01.웹프로젝트/03.구현소스/JS/main.js
+++ 01.웹프로젝트/03.구현소스/JS/main.js
@@ -6,9 +6,10 @@ window.addEventListener("DOMContentLoaded",()=>{
     const btn_n = document.querySelector(".btn_n")
     const btn_p = document.querySelector(".btn_p")
     const gimg = document.querySelector(".gimg");
+    const gimg_ul = document.querySelector(".gimg ul");
     let gimg_li = document.querySelectorAll(".gimg li");
     const img_cnt = gimg_li.length;
-    
+
     
     // search.addEventListener("click",()=>{
     //     top.style.height = "150px";
@@ -16,13 +17,8 @@ window.addEventListener("DOMContentLoaded",()=>{
     // });/////////////// click //////////////////
 
     /////////////////////////// 3pg //////////////////////////////
-    let num = 1;
-    ///////////// 첫번째 사진 나와있기 ////////////////
-    gimg_li.forEach((ele,idx)=>{
-        ele.innerHTML =`
-            <img src="./image/apple/lock_screen_${idx}.jpg" alt="아이폰이미지">
-        `;
-    });
+    
+    
     // gimg2.innerHTML =`
     //     <img src="./image/apple/lock_screen_2.jpg" alt="아이폰이미지">
     // `;
@@ -54,11 +50,15 @@ window.addEventListener("DOMContentLoaded",()=>{
     //     };//////////////////// click /////////////////
     // } ///////////////////// for of ////////////////////////
 
+    /* .gimg li에 이미지 삽입 */
+    gimg_li.forEach((ele,idx)=>{
+        ele.innerHTML =`
+            <img src="./image/apple/lock_screen_${idx}.jpg" alt="아이폰이미지">
+        `;
+    });
 
 
-
-
-    let snum = 0;
+    
 
     gimg_li.forEach((ele,idx)=>{
         ele.setAttribute("data-seq",idx);
@@ -69,12 +69,14 @@ window.addEventListener("DOMContentLoaded",()=>{
         // gimg.insertBefore(gimg_li[gimg_li.length-1],gimg_li[0]);
     };
 
-    for(let i=0;i<1;i++) chgSeq();
+    for(let i=0;i<2;i++) chgSeq();
 
 
     // 광클금지변수 : 0 - 허용, 1 - 불허용
     let prot = 0;
 
+    let snum = 0;
+    
     const goSlide = (seq) => {
         console.log("번호",seq);
 
@@ -88,7 +90,7 @@ window.addEventListener("DOMContentLoaded",()=>{
         if(seq){
             snum++;
             // gimg.appendChild(clist[0])
-            gimg.style.left = "-100%"
+            gimg_ul.style.left = (-100 * snum) + "%";
             console.log("오른쪽",snum);
         }
         else{
@@ -99,12 +101,12 @@ window.addEventListener("DOMContentLoaded",()=>{
         if(snum === -1) snum = img_cnt -1;
         else if(snum === snum) snum = 0;
 
-        chgimg(gimg_li);
     }; //////////// goSlide /////////////
 
     gbtn.forEach((ele,idx)=>{
         ele.onclick = ()=>{
             goSlide(idx);
+            // console.log("new",idx)
         };
     }); /////////// forEach /////////////
 
