f6b518779a705c198872274a6d16cfa8bfc34443
tztx2004 <tztx2004@gmail.com>
2023-03-17 17:20:13 +0900

    updated

--- 01.웹프로젝트/03.구현소스/JS/main.js
+++ 01.웹프로젝트/03.구현소스/JS/main.js
@@ -6,7 +6,8 @@ window.addEventListener("DOMContentLoaded",()=>{
     const btn_n = document.querySelector(".btn_n")
     const btn_p = document.querySelector(".btn_p")
     const gimg = document.querySelector(".gimg");
-    const gimg_li = document.querySelectorAll(".gimg li");
+    let gimg_li = document.querySelectorAll(".gimg li");
+    const img_cnt = gimg_li.length;
     
     
     // search.addEventListener("click",()=>{
@@ -19,7 +20,7 @@ window.addEventListener("DOMContentLoaded",()=>{
     ///////////// 첫번째 사진 나와있기 ////////////////
     gimg_li.forEach((ele,idx)=>{
         ele.innerHTML =`
-            <img src="./image/apple/lock_screen_1.jpg" alt="아이폰이미지">
+            <img src="./image/apple/lock_screen_${idx}.jpg" alt="아이폰이미지">
         `;
     });
     // gimg2.innerHTML =`
@@ -54,14 +55,61 @@ window.addEventListener("DOMContentLoaded",()=>{
     // } ///////////////////// for of ////////////////////////
 
 
-    // 
-    snum = 0;
-    gbtn.forEach((ele,idx)=>{
-        ele.onclick = ()=>{
-            // console.log("hi",idx);
-        };
+
+
+
+    let snum = 0;
+
+    gimg_li.forEach((ele,idx)=>{
+        ele.setAttribute("data-seq",idx);
     });
 
+    const chgSeq = ()=>{
+        gimg_li = document.querySelectorAll(".gimg li");
+        // gimg.insertBefore(gimg_li[gimg_li.length-1],gimg_li[0]);
+    };
 
+    for(let i=0;i<1;i++) chgSeq();
+
+
+    // 광클금지변수 : 0 - 허용, 1 - 불허용
+    let prot = 0;
+
+    const goSlide = (seq) => {
+        console.log("번호",seq);
+
+        // 광클금지 설정하기 //////
+        if (prot) return;
+        prot = 1; // 잠금!
+        setTimeout(() => {
+            prot = 0; // 해제!
+        }, 400); /// 0.4초후 해제! ///
+
+        if(seq){
+            snum++;
+            // gimg.appendChild(clist[0])
+            gimg.style.left = "-100%"
+            console.log("오른쪽",snum);
+        }
+        else{
+            snum--;
+            console.log("왼쪽",snum);
+        }
+
+        if(snum === -1) snum = img_cnt -1;
+        else if(snum === snum) snum = 0;
+
+        chgimg(gimg_li);
+    }; //////////// goSlide /////////////
+
+    gbtn.forEach((ele,idx)=>{
+        ele.onclick = ()=>{
+            goSlide(idx);
+        };
+    }); /////////// forEach /////////////
+
+    function chgimg(obj){
+        obj.forEach((ele)=>ele.style.left = "100%");
+    }
 
 }); ////////////////////// 로드구역 ///////////////////////////////////
