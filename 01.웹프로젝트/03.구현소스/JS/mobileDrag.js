8b375208b5a8365bf36365faa4b724911adb1e81
tztx2004 <tztx2004@gmail.com>
2023-03-29 11:58:19 +0900

    mobileDrag

--- 01.웹프로젝트/03.구현소스/JS/mobileDrag.js
+++ 01.웹프로젝트/03.구현소스/JS/mobileDrag.js
@@ -38,7 +38,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 let moveY = e.changedTouches[0].clientY;
                 m_moveX = moveX - m_firstX;
 
-                ele.style.left = -380 + m_moveX + "px";
+                ele.style.left = m_moveX + "px";
                 // if(m_moveX < - 50) ele.style.left = -100 + "px";
                 // else if(m_moveX > 50 ) ele.style.left = 100 + "px";
                 // else if(m_moveX < 50 && m_moveX > -50) ele.style.left = 0+ "px";
@@ -57,53 +57,33 @@ window.addEventListener("DOMContentLoaded", () => {
             let tgLeft = obj.offsetLeft;
             // let tgPoint = obj.parentElement.clientWidth;
             console.log("tgL", tgLeft);
-
-            if (tgLeft <= -winW * 1.1) {
-                // console.log("요기");
+            // 오른쪽으로 이동
+            // if (tgLeft <= -winW * 1.1) {
+            if (m_moveX <= -50) {
                 obj.style.transition = ".4s";
-                obj.style.left = -winW * 2 + "px";
-
-                setTimeout(() => {
-                    obj.appendChild(obj.querySelectorAll("li")[0]);
-                    obj.style.transition = "none";
-                    obj.style.left = "-100%";
-                }, 400);
-                // slide_li.forEach((aa,idx)=>{
-                //     console.log(idx)
-                // })
-                // ele.appendChild(slide_li[0])
-                // ele.style.left = -100*(snum+1) + "%";
-                // slide_li.forEach((ele, idx) => {
-                // });
-                // console.log("snum",snum);
-            } else if (tgLeft >= -winW * 0.9) {
-                let ltg = obj.querySelectorAll("li");
+                obj.appendChild(obj.querySelectorAll("li")[0]);
                 obj.style.transition = "none";
-                obj.insertBefore(ltg[ltg.length-1],ltg[0]);
+                obj.style.left = "0%";
+            }
+            // 왼쪽으로 이동
+            // else if (tgLeft >= -winW * 0.9) {
+            else if (m_moveX >= 50) {
+                let ltg = obj.querySelectorAll("li");
                 tgLeft = obj.offsetLeft
-                console.log(tgLeft);
-                obj.style.left = (-winW+tgLeft)+"px";
-
-                setTimeout(() => {
-                    obj.style.transition = ".4s";
-                    obj.style.left = "-100%";
-                }, 1);
-            } else {
-                
                 obj.style.transition = ".4s";
-                    obj.style.left = "-100%";
+                obj.insertBefore(ltg[ltg.length-1],ltg[0]);
+                obj.style.left = "0%";
+                obj.style.transition = "none";
+            }
+            // 제자리
+            else {
+                obj.style.transition = ".4s";
+                obj.style.left = "0%";
             }
             // ele.style.transition = ".4s";
             // ele.style.left = -100 * sno + "%";
         }
-        // function goSlide(seq){
-        //     if(seq === 1){
-        //         ele.style.left = "-100%";
-        //     }
-        // }
     } /////////////////// mobileDrag //////////////////
-    // let snum = 0;
-    // if(snum>=0)snum++;
 
     // 호출
     mobileDrag(slide);
