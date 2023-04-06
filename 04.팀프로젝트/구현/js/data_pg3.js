973f853144a20cd10caee28884abcfa571e5d195
tztx2004 <tztx2004@gmail.com>
2023-04-06 09:31:46 +0900

    updated

---
+++ 04.팀프로젝트/구현/js/data_pg3.js
@@ -0,0 +1,18 @@
+const mars_data = {
+    "Curiosity" : {
+        "제목" : "What NASA’s Curiosity Rover Has Learned in Over 10 Years of Exploring",
+        "기사" : "NASA’s Curiosity Mars rover set out to answer a big question when it landed on the Red Planet more than 10 years ago: Could Mars have supported ancient life? Scientists have discovered the answer is yes, and they have been working to learn more about the planet’s past habitable environment."
+    },
+    "Maven":{
+        "제목":"MAVEN Explores Mars to Understand Radio Interference at Earth",
+        "기사":"NASA’s MAVEN mission explores the atmosphere of Mars to better study a phenomenon observed at Earth, known as &quot;Sporadic-E Layers.&quot; Learn more in this comic book-style animated video."
+    },
+    "Rover":{
+        "제목":"NASA’s Curiosity Mars Rover Finds A Changing Landscape",
+        "기사":"Take a look at the view from inside Gale crater as NASA’s Curiosity Mars Rover explores a changing landscape."
+    },
+    "Odyssey":{
+        "제목":"Odyssey",
+        "기사":"2001 Mars Odyssey is a robotic spacecraft orbiting the planet Mars. The project was developed by NASA, and contracted out to Lockheed Martin, with an expected cost for the entire mission of US$297 million."
+    },
+};
