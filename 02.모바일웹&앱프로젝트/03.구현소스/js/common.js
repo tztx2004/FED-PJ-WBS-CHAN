8eff1b873622a003a1469cb0dc105c2037e704cc
tztx2004 <tztx2004@gmail.com>
2023-04-23 23:17:41 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
@@ -1,3 +1,22 @@
 // JS
+$(()=>{
+    console.log("hi")
+    const top = $(".top");
 
-console.log("hi")
+    
+    // addEventListener("scroll",(e)=>{
+    //     const direction = e.deltaY > 0 ? "Scroll Down" : "Scroll Up";
+    //     // 방향과 현 스크롤 위치
+    //     console.log(direction, window.scrollY);
+    // });
+
+    window.addEventListener('scroll', e => {
+        const scrollUp = e.deltaY <= 0;
+        console.log(window.scrollY)
+        if ( window.scrollY > 100 &&  scrollUp) {
+            $(top).addClass('scrollUp');
+        } else {
+            $(top).removeClass('scrollUp');
+        }
+    });
+});/////////////////////////////////jQB//////////////////////////////////////////
