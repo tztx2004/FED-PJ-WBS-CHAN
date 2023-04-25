7752e1a0e016fe5a8fa1a72c203db586fd43aabc
tztx2004 <tztx2004@gmail.com>
2023-04-25 14:46:05 +0900

    230425 오후

--- 02.모바일웹&앱프로젝트/03.구현소스/js/test.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/test.js
@@ -2,9 +2,32 @@ $(()=>{
     
 
     /* 양쪽글씨뒤집기 */
-    let rt1 = $(".rotate-text");
+    let rt = $(".rotate-text");
+    let rt1 = $(".rotate-text1");
     let rt2 = $(".rotate-text2");
 
     console.log("hi",rt2)
+
+    $(window).on("scroll",()=>{
+        
+
+        let ws = $(window).scrollTop()
+        let tg = rt1.offset().top;
+        console.log("ws",ws,"tg",tg)
+        if(ws > tg-700 && ws +300){
+            $(rt).addClass("on")
+            $(rt).html(`school`);
+
+
+            // $(rt1).css({
+            //     transform:
+            //     `rotate(-90deg) rotateX(${ws/10-110}deg) translateY(133px)`
+            // })
+            // $(rt2).css({
+            //     transform:
+            //     `rotate(-90deg) rotateX(${ws/10}deg) translateY(133px)`
+            // })
+        }
+    })
 
 }); /////// load  //////
