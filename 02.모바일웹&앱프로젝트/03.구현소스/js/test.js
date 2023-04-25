5cbdcf44623130739f848810241f1875bf2490f0
tztx2004 <tztx2004@gmail.com>
2023-04-25 14:00:40 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/test.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/test.js
@@ -1,3 +1,10 @@
 $(()=>{
-    console.log("hi")
+    
+
+    /* 양쪽글씨뒤집기 */
+    let rt1 = $(".rotate-text");
+    let rt2 = $(".rotate-text2");
+
+    console.log("hi",rt2)
+
 }); /////// load  //////
