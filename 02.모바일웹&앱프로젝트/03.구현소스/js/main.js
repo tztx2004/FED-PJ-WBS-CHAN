58b8c4fabf82faa797f028e733a063a2d05a0912
tztx2004 <tztx2004@gmail.com>
2023-04-27 17:20:39 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/main.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/main.js
@@ -6,9 +6,21 @@ const pg3_tit2 = $(".pg3_tit2 h3");
 const pg3_items = $(".pg3_items");
 
 
+// 휠이벤트 //
 window.addEventListener("wheel",()=>{
+    // 글씨회전 함수(현재는 메인 3페이지에서 구현)
+    rotFont();
+    
+    // 가로스크롤 (임시로 호출시켜놓음)
+    hrScroll();
+
+})/////////////// wheel //////////////////
+
+
+function rotFont(){ // 글씨회전 함수
     let delta = event.deltaY>0
     let ot;
+
     pg3_items.each((idx,ele)=>{
         ot = $(ele).offset().top;
         let ws = window.scrollY;
@@ -31,8 +43,8 @@ window.addEventListener("wheel",()=>{
                 $(pg3_tit2).css({
                     transform: `rotate(90deg) rotateX(${ws/10-190}deg) translateY(50%)`,
                 });
-            }
-        }
+            }//////////// if //////////////
+        }//////////////// if //////////////////
         else{ // 스크롤 업하면서 컨텐츠만나면 글씨변함
             $(pg3_tit1).css({
                 transform: `rotate(-90deg) rotateX(${ws/10-190}deg) translateY(50%)`,
@@ -42,9 +54,19 @@ window.addEventListener("wheel",()=>{
                 transform: `rotate(90deg) rotateX(${ws/10-190}deg) translateY(50%)`,
                 transtition:".6s easeout"
             });
-        }
+        } /////////// else ////////////
     });////////////// each ///////////////
-    
-    
+} ////////////////// rotFont_글씨회전 /////////////////
 
-})/////////////// wheel //////////////////
+
+
+// 가로스크롤
+function hrScroll(){
+    // 스크롤 변수
+
+    const pg2_cont = $(".pg2_containers");
+    pg2_cont.css({
+        left:``
+    })
+
+}/////////////// hrScroll ///////////////////
