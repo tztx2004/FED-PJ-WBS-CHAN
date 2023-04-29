8d1564130e8af7b8af86e9b0153f4777783672c9
tztx2004 <tztx2004@gmail.com>
2023-04-29 18:21:44 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/main.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/main.js
@@ -4,17 +4,72 @@ const pg3_tit1 = $(".pg3_tit1 h3");
 const pg3_tit2 = $(".pg3_tit2 h3");
 
 const pg3_items = $(".pg3_items");
-
+const main_imgbx = $(".main_imgbx");
 
 // 휠이벤트 //
 window.addEventListener("wheel",()=>{
     // 글씨회전 함수(현재는 메인 3페이지에서 구현)
-    rotFont();
+    // rotFont();
     
+    // 새버전 글씨회전
+    rotF();
+
     // 가로스크롤 (임시로 호출시켜놓음)
     hrScroll();
 
 })/////////////// wheel //////////////////
+
+let wds = ["YaHO","Goooooods","LALALA"]; // pg3에 들어갈 말
+function rotF(){
+    let delta = event.deltaY>0
+    let ot;
+    let pos = window.scrollY; // 현재 스크롤위치
+
+    main_imgbx.each((idx,ele)=>{
+        ot = $(ele).offset().top; // 그림 위치
+        let elH = $(ele).height();
+
+        if(pos>=ot && pos< ot+ elH){
+            cls(pg3_tit1,wds[idx])
+        }
+        else if(pos>= ot+ elH){
+            rev_on(pg3_tit1)
+        }
+        // console.log(pos,ot+elH);
+    });/////////////////// each /////////////////////
+
+    
+    // 1. 내 위치를 구한다
+    // 2. 각 사진의 offset().top값을 구한다
+    // 3. 내 위치가 offset().top값을 넘어가면 클레스를 준다
+    // 4. 기존 클래스를 지우고 다시 on, on2를 넣어준다
+
+}//////////// rotF //////////////////
+
+// on, on2클래스 줌
+function cls(x,words){
+    x.css({
+        transition:"6s ease-out"
+    });
+
+    // 90도 돌아
+    x.addClass("on")
+    // 돌고 글자 변경
+    setTimeout(() => {
+        x.html(words);
+    }, 1000);
+    // 360도 회전
+    x.addClass("on2")
+}
+
+// on, on2 클래스 뺌
+function rev_on(x){
+    x.css({
+        transition:"none"
+    })
+    x.removeClass("on");
+    x.removeClass("on2");
+}
 
 
 function rotFont(){ // 글씨회전 함수
