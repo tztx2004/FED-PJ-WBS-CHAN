0c5e656d858a3a3fe06787155628313ac608f8fa
tztx2004 <tztx2004@gmail.com>
2023-05-01 13:19:49 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
@@ -1,23 +1,32 @@
 // JS
-$(()=>{
-    console.log("hi")
-    const top = $(".top");
+console.log("common")
+// 대상
+const top = $(".top");
+const gnb_ol = $(".gnb_ol");
+const gnb_li = $(".gnb_ol li");
+const gnb_arr = ["Product","Service","Support","Contact"]
 
-    
-    // addEventListener("scroll",(e)=>{
-    //     const direction = e.deltaY > 0 ? "Scroll Down" : "Scroll Up";
-    //     // 방향과 현 스크롤 위치
-    //     console.log(direction, window.scrollY);
-    // });
+// gnb 출력
+gnb_li.each(function(idx,ele){
+    $(ele).html(`
+        <a href="#">${gnb_arr[idx]}</a>
+    `)
+});///////// each ////////////
 
-    // top영역 스크롤 시 사라짐
-    window.addEventListener('wheel', e => {
-        const scrollUp = e.deltaY > 0;
-        // console.log(scrollUp)
-        if (scrollUp) {// 스크롤다운
-            $(top).addClass('on');
-        } else {// 스크롤 업
-            $(top).removeClass('on');
-        }
-    });
-});/////////////////////////////////jQB//////////////////////////////////////////
+
+// addEventListener("scroll",(e)=>{
+//     const direction = e.deltaY > 0 ? "Scroll Down" : "Scroll Up";
+//     // 방향과 현 스크롤 위치
+//     console.log(direction, window.scrollY);
+// });
+
+// top영역 스크롤 시 사라짐
+window.addEventListener('wheel', e => {
+    const scrollUp = e.deltaY > 0;
+    // console.log(scrollUp)
+    if (scrollUp) {// 스크롤다운
+        $(top).addClass('on');
+    } else {// 스크롤 업
+        $(top).removeClass('on');
+    }
+});
