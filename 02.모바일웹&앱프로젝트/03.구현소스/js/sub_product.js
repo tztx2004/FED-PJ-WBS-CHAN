bedfb9d73f0feda6e26aa558618466932e7a60d3
tztx2004 <tztx2004@gmail.com>
2023-05-06 18:02:32 +0900

    updated

--- 02.모바일웹&앱프로젝트/03.구현소스/js/sub_product.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/sub_product.js
@@ -12,3 +12,27 @@ new Vue({
     created(){},
     mounted(){}
 }); ////////////////// vue 인스턴스 ////////////////
+
+// let nuSrc;
+// let oriSrc = $(".item_pic img").each(function(idx,ele){
+//     $(ele).attr("src");
+// })
+// console.log('오리 전',oriSrc);
+// $(".item_pic img").hover(
+//     function(){
+//         nuSrc = `./img/main/sub_product/0ac04533-58e4-4b73-b05c-4e8c66d5ddc7.jpeg`;
+//         $(this).attr("src",nuSrc);
+//         console.log("before",$(this).attr("src"))
+//     },
+//     function(){
+//         console.log("origin 후",oriSrc)
+//         $(this).attr("src",oriSrc);
+//         console.log("after",$(this).attr("src"))
+//     }
+// );////////////////// hover //////////////////
+
+$(".item_pic img").hover(function(){
+    $(this).attr("src", $(this).attr("src").replace(".jpeg", "_on.jpeg")); 
+}, function(){ 
+    $(this).attr("src", $(this).attr("src").replace("_on.jpeg", ".jpeg")); 
+}); 
