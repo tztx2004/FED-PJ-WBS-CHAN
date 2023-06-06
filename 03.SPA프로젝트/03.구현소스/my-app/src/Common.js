ae436a182ed2ee833c8e0c6bb10f9fafd20a4e1f
tztx2004 <tztx2004@gmail.com>
2023-06-07 00:44:18 +0900

    updated

---
+++ 03.SPA프로젝트/03.구현소스/my-app/src/Common.js
@@ -0,0 +1,23 @@
+import './common.css';
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
+
+
+
+function Common() {
+  return (
+    <>
+      <header className='header'>
+        <nav className='nav limitWidth'>
+          <Link to={"/MODELS"}>
+          <span className='Modles nav_text'>MODLES</span>
+          </Link>
+          <span className='Main nav_text'>AI MODELS</span>
+          <span className='Contact nav_text'>CONTACT</span>
+        </nav>
+        <span className='header_text'>DO AI MODELS DREAM OF ELECTRIC SHEEP?</span>
+      </header>
+    </>
+  );
+}
+
+export default Common;
