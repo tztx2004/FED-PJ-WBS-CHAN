ce23083feffbb1c0e1f700e6cb0ac07caa25c487
tztx2004 <tztx2004@gmail.com>
2023-06-09 17:20:01 +0900

    updated

--- 03.SPA프로젝트/03.구현소스/my-app/src/components/main2pg.js
+++ 03.SPA프로젝트/03.구현소스/my-app/src/components/main2pg.js
@@ -1,7 +1,28 @@
+import main_data from "../data/main_data";
+import "../css/main.css";
+import $ from 'jquery';
 
 const Main2pg = function(){
     return(
-        console.log("2v")
+        <>
+            <div className="ma2_2pg_about">
+                <div className="ma_2pg_img">
+                    <div className="about">
+                        <div className="about_cover">
+                            <img src="./images/main/about_frame_pc.jpg" alt="이미지"/>
+                        </div>
+                        <img src="./images/main/aboutA_pc@1.5x.jpg" alt="이미지"/>
+                    </div>
+                    <div className="about1"></div>
+                    <div className="about2"></div>
+                    <div className="about3"></div>
+                </div>
+                <div className="ma_2pg_copy">
+                    <div className="ma_2pg_title"></div>
+                    <div className="ma_2pg_cont"></div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- 03.SPA프로젝트/03.구현소스/my-app/src/data/main_data.js
+++ 03.SPA프로젝트/03.구현소스/my-app/src/data/main_data.js
@@ -3,6 +3,10 @@ let main_data = {
     pg1:{
         title:"WE SOLVE ISSUES IN FASHION BUSINESSES WITH AI TECHNOLOGY AND CREATIVE TO CREATE NEW WORTH WITH GLOBAL APPAREL COMPANIES.",
         font:"We solve fashion business issues with AI technology and creativity,^ and create new value with apparel companies around the world."
+    },
+    pg2:{
+        title:"BY GENERATING MODELS WITH OUR UNIQUE AI TECHNOLOGY, WE OFFER FASHION MODELS THAT ARE PERFECTLY OPTIMISED FOR THE IDENTITY OF APPAREL BRANDS.",
+        font:"We provide fashion models optimized for apparel brands."
     }
 }
 
