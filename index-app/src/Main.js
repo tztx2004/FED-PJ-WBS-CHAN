d689a9ead150c99118ed22bae07d32e72d878612
tztx2004 <tztx2004@gmail.com>
2023-06-09 23:39:23 +0900

    updated

---
+++ index-app/src/Main.js
@@ -0,0 +1,71 @@
+import './css/App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import main_data from './main_data';
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="logo">FRONT-END CHAN</div>
+
+        <nav>
+          <Link to="#1">
+            <div>INTRODUCTION</div>
+          </Link>
+          {/* <Link to="#2">
+            <div></div>
+          </Link> */}
+          <Link to="#2">
+            <div>PORTFOLIO</div>
+          </Link>
+        </nav>
+
+      </header>
+      <main className="main">
+        <section id='1' className='pg1'>
+          <div className='pg1_wrap'>
+            <div className='my_pic'>
+              <img src="./logo192.png" alt="이미지"/>
+            </div>
+            <div className='about_me'>
+              <h1>
+                안녕하세요! 프론트엔드 웹개발자 <br/>
+                <strong>정찬</strong>입니다!
+              </h1>
+              <h2>
+                다양한 주제로 웹페이지를 제작하고 있는 프론트엔드 웹개발자 정찬입니다!<br/>
+                javascript, Vue.js, React, Html, CSS로 웹개발하고 있습니다!<br/>
+                학습능력이 좋은편이기 때문에 새로운 프레임워크, 라이브러리에 적응을 잘합니다!<br/></h2>
+              
+            </div>
+          </div>
+        </section>
+        <section id='2' className='pg2'>
+          <div className='pg2_wrap'>
+            {
+              main_data.map((x,i)=>
+                <a className='port_wrap' href={x.psrc} target="_blank">
+                  <div className='portbx'>
+                    <div className='portImg'>
+                      <img src={x.pimg} alt="이미지"/>
+                    </div>
+                    <div className='portTitle'>{x.ptit}</div>
+                    <div className='portInfo'>{x.pinfo}</div>
+                  </div>
+                </a>
+              )
+            }
+          </div>
+        </section>
+        <section id='3' className='pg3'></section>
+      </main>
+      <section className='info'>
+
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ index-app/src/main_data.js
@@ -0,0 +1,33 @@
+const main_data = [
+    { // apple
+        psrc:"https://tztx2004.github.io./FED-PJ-WBS-CHAN/01.%EC%9B%B9%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/apple.html",
+        pimg:"./images/apple.png",
+        ptit:"Apple iPhone14 pro",
+        pinfo:`애플사의 아이폰을 주제로 웹페이지를 제작하였습니다${<br/>}
+        사용된 언어는 JavaScript이며, 다양한 효과를 입히는 것에 노력했습니다!
+        `,
+    },
+    { // nasa
+        psrc:"https://tztx2004.github.io./FED-PJ-WBS-CHAN/04.%ED%8C%80%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/solarsystem/main.html",
+        pimg:"./images/nasa.png",
+        ptit:"NASA",
+        pinfo:`nasa를 주제로 여러 행성들을 소개하는 페이지 입니다.${<br/>}
+        팀프로젝트로 진행하였으며, 메인페이지와 Mars페이지를 제작하였습니다.${<br/>}
+        사용된 언어는 JavaScript입니다.
+        `,
+    },
+    { // musium
+        psrc:"https://tztx2004.github.io./FED-PJ-WBS-CHAN/02.%EB%AA%A8%EB%B0%94%EC%9D%BC%EC%9B%B9&%EC%95%B1%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/index.html",
+        pimg:"./images/musium.png",
+        ptit:"Musium",
+        pinfo:"그림을 판매하는 페이지를 제작하였습니다. 실제 판매를 하고있지는 않으며, Vue.js와 jQuery를 통해 제작하였습니다.",
+    },
+    { // ai
+        psrc:"https://www.ai-model.jp/",
+        pimg:"./images/ai.png",
+        ptit:"AI MODELS(제작중)",
+        pinfo:`AI모델을 소개하는 페이지입니다. ReactJS로 제작 중이며 연결된 링크는 이번 제작에 대상으로 선정된 페이지입니다.`,
+    },
+];
+
+export default main_data;
