7278202ff3139d72ce16dea7721065797210f0d9
tztx2004 <tztx2004@gmail.com>
2023-06-13 12:33:00 +0900

    updated

--- 03.SPA프로젝트/03.구현소스/my-app/src/components/main2pg.js
+++ 03.SPA프로젝트/03.구현소스/my-app/src/components/main2pg.js
@@ -10,10 +10,14 @@ $(()=>{
     const cv1 =$(".cv1")
     const cv2 =$(".cv2")
     const cv3 =$(".cv3")
+    const cv4 =$(".cv4")
+    const cv5 =$(".cv5")
+    const cv6 =$(".cv6")
     cover.css({
         backgroundSize:"300%",
         backgroundAttachment:"fixed"
     })
+    
 
     // 사진 필터링
     function filterImg(ty){ // ty - pc나 sp(type)
@@ -32,6 +36,15 @@ $(()=>{
         cv3.css({
             backgroundPosition:'100% 0',
         })
+        cv4.css({
+            backgroundPosition:'150% 0',
+        })
+        cv5.css({
+            backgroundPosition:'200% 0',
+        })
+        cv6.css({
+            backgroundPosition:'250% 0',
+        })
     }
     filterImg("pc");
 
@@ -49,10 +62,10 @@ $(()=>{
         let pg2W = pg2.width();
         // console.log("sss",pg2W)
         pg2.css({
-            height:`${pg2W * 3}px`
+            height:`${pg2W * (cover.length-3.5)}px`
         }) //// css /////
 
-        if(curSc > start2pg){
+        if(curSc > start2pg && curSc < pg2.height()){
             let mp = Math.ceil(curSc - start2pg);
             // console.log("mp",mp)
             // console.log("start2pg",start2pg,"cr",curSc)
@@ -63,7 +76,6 @@ $(()=>{
             console.log(-50-mp/10)
             cover.css({
                 transform:`translateX(${-mp/10}%)`,
-                
             })/// css ///
             cv1.css({
                 backgroundPosition:`${-mp/20}% 0`,
@@ -73,6 +85,15 @@ $(()=>{
             })
             cv3.css({
                 backgroundPosition:`${100-mp/20}% 0`,
+            })
+            cv4.css({
+                backgroundPosition:`${150-mp/20}% 0`,
+            })
+            cv5.css({
+                backgroundPosition:`${200-mp/20}% 0`,
+            })
+            cv6.css({
+                backgroundPosition:`${250-mp/20}% 0`,
             })
             
         } /// if ///
@@ -90,21 +111,26 @@ const Main2pg = function(){
                         <div className="about_cover cv1"></div>
                         <div className="about_cover cv2"></div>
                         <div className="about_cover cv3"></div>
-                        {/* <div className="cup">
-                            <img src="./images/main/aboutA_pc@1.5x.jpg" alt="이미지" className="cupImg"/>
-                        </div> */}
-                        <img src="./images/main/aboutA_pc@1.5x.jpg" alt="이미지"/>
-                        <img src="./images/main/aboutB_pc@1.5x.jpg" alt="이미지"/>
-                        <img src="./images/main/aboutC_pc@1.5x.jpg" alt="이미지"/>
+                        <div className="about_cover cv4"></div>
+                        <div className="about_cover cv5"></div>
+                        <div className="about_cover cv6"></div>
                     </div>
-                    <div className="about1">TEST</div>
+                    <div className="about1"></div>
                     <div className="about2"></div>
                     <div className="about3"></div>
                 </div>
             </div>
             <div className="ma_2pg_copy">
-                <div className="ma_2pg_title">test2</div>
-                <div className="ma_2pg_cont"></div>
+                <div className="ma_2pg_title">
+                    THE MODELS
+                </div>
+                <div className="ma_2pg_cont">
+                    <span className="numb">01</span>
+                    <div className="ma_2pg_copy">
+                        <h2>{main_data.pg2.title}</h2>
+                        <h3>{main_data.pg2.font}</h3>
+                    </div>
+                </div>
             </div>
 
         </>
